b9c2cd8b0d9ca1edcee085da1f60a7d3eab2208d
akhmadas25 <akhmadsyaifudin2508@gmail.com>
2021-12-11 07:06:03 +0700

    done ticket app

--- index.js
+++ index.js
@@ -1,195 +1,191 @@
 // import package
-const http = require("http")
-const express = require("express")
-const path = require("path")
-const session = require('express-session')
-const app = express()
-const hbs = require("hbs")
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const app = express();
+const hbs = require("hbs");
 // handle location of file
-var pathFile = 'http://localhost:5000/uploads/'
+var pathFile = "http://localhost:5000/uploads/";
 
 // routing
-const backendRoute = require("./routes/backend")
-const processRoute = require("./routes/process")
+const backendRoute = require("./routes/backend");
+const processRoute = require("./routes/process");
 
 // set connection & middlewares
-const dbConnection = require('./connection/database')
-const uploadFile = require('./middlewares/uploadFile')
-const { response } = require("express")
-const isLogin = false
+const dbConnection = require("./connection/database");
+const uploadFile = require("./middlewares/uploadFile");
+const { response } = require("express");
+const isLogin = false;
 
 // set public
-app.use("/public", express.static(path.join(__dirname, "public")))
+app.use("/public", express.static(path.join(__dirname, "public")));
 // set uploaded file
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-// set views location to app 
-app.set("views", path.join(__dirname, "views"))
+// set views location to app
+app.set("views", path.join(__dirname, "views"));
 
 // set template/view engine
-app.set("view engine", "hbs")
+app.set("view engine", "hbs");
 
 // register view partials
-hbs.registerPartials(path.join(__dirname, "views/partials"))
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 // use session
 app.use(
-  session(
-    {
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 2,
-      },
-      store: new session.MemoryStore(),
-      resave: false,
-      saveUninitialized: true,
-      secret: 'SecretKey',
-    }
-  )
-)
+  session({
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 2,
+    },
+    store: new session.MemoryStore(),
+    resave: false,
+    saveUninitialized: true,
+    secret: "SecretKey",
+  })
+);
 
 // session alert
-app.use(function(req,res,next){
-  res.locals.message = req.session.message
-  delete req.session.message
-  next()
-})
+app.use(function (req, res, next) {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
 
 // render index page
-app.get("/", function(request, res) {
-  const query = `SELECT * FROM tb_movie`
+app.get("/", function (request, res) {
+  const query = `SELECT * FROM tb_movie`;
 
-  dbConnection.getConnection(function (err,conn) {
-    if (err) throw err
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
     conn.query(query, function (err, results) {
-      if (err) throw err
-      const movies = []
+      if (err) throw err;
+      const movies = [];
 
       for (var result of results) {
         movies.push({
           id: result.id,
           name: result.name,
           duration: result.movie_hour,
-          poster: pathFile + result.poster
-        })
+          poster: pathFile + result.poster,
+        });
       }
-      res.render("index", {title: "Welcome to XXI Tegal", 
-      isLogin: request.session.isLogin,
-      movies,
-      })
-    })
-  })
-})
+      res.render("index", {
+        title: "Welcome to XXI Tegal",
+        isLogin: request.session.isLogin,
+        movies,
+      });
+    });
+  });
+});
 
 // render signup
-app.get('/signup', function(request,response) {
-  const title = 'signup';
-  response.render('auth/signup', {
-    title
+app.get("/signup", function (request, response) {
+  const title = "signup";
+  response.render("auth/signup", {
+    title,
   });
-})
+});
 // handle signup
-app.post('/signup', function(request,response) {
-  var {email, password} = request.body;
+app.post("/signup", function (request, response) {
+  var { email, password } = request.body;
 
-  if (email == '' ||  password == '') {
+  if (email == "" || password == "") {
     request.session.message = {
-      type: 'danger',
-      message: 'Please insert all field!',
+      type: "danger",
+      message: "Please insert all field!",
     };
-    return response.redirect('/signup');
+    return response.redirect("/signup");
   }
-    const query = `INSERT INTO tb_user (email, password) VALUES ("${email}", "${password}")`;
-    dbConnection.getConnection(function(err,conn){
-      if(err) throw err;
-        conn.query(query,function(err,result) {
-          if (err) throw err;
+  const query = `INSERT INTO tb_user (email, password) VALUES ("${email}", "${password}")`;
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, result) {
+      if (err) throw err;
 
-          request.session.message = {
-            type: 'success',
-            message: 'successfully signup!',
-          };
+      request.session.message = {
+        type: "success",
+        message: "successfully signup!",
+      };
 
-          response.redirect('/signup');
-        });
+      response.redirect("/signup");
+    });
 
-        conn.release();
-     });
-})
+    conn.release();
+  });
+});
 // render login
-app.get('/login', function(request,response) {
-  const title = 'Login';
-  response.render('auth/login', {
+app.get("/login", function (request, response) {
+  const title = "Login";
+  response.render("auth/login", {
     title,
     isLogin,
   });
-})
+});
 // handle login
-app.post('/login', function(request,response) {
+app.post("/login", function (request, response) {
   const { email, password } = request.body;
 
-  if(email == '' || password == ''){
+  if (email == "" || password == "") {
     request.session.message = {
-      type: 'danger',
-      message: 'Please insert all field!',
+      type: "danger",
+      message: "Please insert all field!",
     };
 
-    return response.redirect('login');
+    return response.redirect("login");
   }
 
   const query = `SELECT * FROM tb_user WHERE email = "${email}" AND password = "${password}"`;
-  dbConnection.getConnection(function(err,conn){
-    if(err) throw err;
-    conn.query(query,function(err,results) {
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, results) {
       if (err) throw err;
- 
+
       if (results.length == 0) {
         request.session.message = {
-          type: 'danger',
-          message: 'Email and password dont match!',
+          type: "danger",
+          message: "Email and password dont match!",
         };
-        return response.redirect("login")
-// handle user
+        return response.redirect("login");
+        // handle user
       } else {
-
         request.session.isLogin = true;
 
         request.session.user = {
           id: results[0].user_id,
           email: results[0].email,
-          status: results[0].status
-        }
-        const user = results[0]
-        let status = user.status
-        request.session.isAdmin = false
-        if (status == "admin"){
-           request.session.isAdmin = true
-          response.redirect('/admin')
-        } 
-        else if(status == "user") {
-          request.session.isAdmin = false
-          response.redirect('/')
+          status: results[0].status,
+        };
+        const user = results[0];
+        let status = user.status;
+        request.session.isAdmin = false;
+        if (status == "admin") {
+          request.session.isAdmin = true;
+          response.redirect("/admin");
+        } else if (status == "user") {
+          request.session.isAdmin = false;
+          response.redirect("/");
         }
       }
     });
     conn.release();
   });
-})
+});
 // handle signout
-app.get('/signout', function(request,response) {
+app.get("/signout", function (request, response) {
   request.session.destroy();
-  response.redirect('/');
-})
-
+  response.redirect("/");
+});
 
 // mount backend page
-app.use("/", backendRoute)
+app.use("/", backendRoute);
 // mount proses route
-app.use("/", processRoute)
+app.use("/", processRoute);
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 const port = 5000;
 server.listen(port, () => {
-  console.log('server running on port: ', port)
-})
+  console.log("server running on port: ", port);
+});
--- routes/backend.js
+++ routes/backend.js
@@ -1,195 +1,16 @@
 // handle upload image
-const uploadFile = require('../middlewares/uploadFile');
+const uploadFile = require("../middlewares/uploadFile");
 
-const router = require('express').Router()
+const router = require("express").Router();
 // handle connection
-const dbConnection = require('../connection/database');
+const dbConnection = require("../connection/database");
 // handle location of file
-var pathFile = 'http://localhost:5000/uploads/';
+var pathFile = "http://localhost:5000/uploads/";
 
-
-router.get("/admin", function(request, response) {
+router.get("/admin", function (request, response) {
   const query = `SELECT * FROM tb_movie`;
 
-  dbConnection.getConnection(function (err,conn) {
-    if (err) throw err;
-    conn.query(query, function (err, results) {
-      if (err) throw err;
-      const movies = [];
-
-      for (var result of results) {
-        movies.push({
-          id: result.id,
-          name: result.name,
-          duration: result.movie_hour,
-          poster: pathFile + result.poster
-        });
-      }
-
-      if (request.session.isAdmin){
-        response.render("admin/dashboard", {
-        title: "dashboard", 
-        isAdmin: request.session.isAdmin,
-        movies
-      })
-      }
-      else {
-        response.redirect('/login')
-      }
-      conn.release()
-      });
-    });
-})
-
-router.get("/reports", function(request, response){
-  const query = `SELECT t.id, t.ticket_number, t.date_show, p.sub_total, m.name, u.email
-  FROM tb_ticket t
-  INNER JOIN tb_movie m
-  ON t.movie_id = m.id
-  INNER JOIN tb_payment p
-  ON t.id = p.ticket_id
-  INNER JOIN tb_user u
-  ON t.user_id = u.user_id;`
-
-  dbConnection.getConnection(function (err,conn) {
-    if (err) throw err;
-    conn.query(query, function (err, results) {
-      if (err) throw err;
-      const reports = []
-
-      for (var result of results) {
-        reports.push({
-          id: result.id,
-          ticket: result.ticket_number,
-          name: result.name,
-          date: result.date_show,
-          email: result.email,
-          total: result.sub_total
-        });
-      }
-      if (request.session.isAdmin){
-        response.render("admin/reports", {
-        title: "dashboard", 
-        isAdmin: request.session.isAdmin,
-        reports
-      })
-      }
-      else {
-        response.redirect('/login')
-      }
-      conn.release()
-      })
-    })
-
-})
-
-router.get('/addMovies', function(request, response){
-
-  const query = `SELECT id, name FROM tb_type`
-
-  dbConnection.getConnection(function (err,conn) {
-    if (err) throw err
-    conn.query(query, function (err, results) {
-      if (err) throw err
-      const type = []
-
-      for (var result of results) {
-        type.push({
-          id: result.id,
-          name: result.name,
-        });
-      }
-
-      if (request.session.isAdmin){
-        response.render("admin/addMovies", {
-        title: "add new movies", 
-        isAdmin: request.session.isAdmin,
-        type
-      })
-      }
-      else {
-        response.redirect('/login')
-      }
-
-      conn.release()
-      })
-    })
-})
-
-router.post('/addMovies', uploadFile('image'), function(request, response){
-  const {movie_title, duration, sinopsis, category} = request.body
-  var image = '';
-
-// handle file
-  if (request.file){
-    image = request.file.filename;
-  }
-
-  if(movie_title == '' || image == '' || duration == ''){
-    request.session.message = {
-      type: 'danger',
-      message: 'Please insert all field!',
-    };
-
-    response.redirect('/addMovies');
-  }
-
-  const query = `INSERT INTO tb_movie (name, movie_hour, poster, sinopsis, type_id) VALUES ("${movie_title}", "${duration}", "${image}", "${sinopsis}", "${category}")`
-  dbConnection.getConnection(function(err,conn){
-    if(err) throw err
-      conn.query(query,function(err,result) {
-        if (err) throw err
-
-        request.session.message = {
-          type: 'success',
-          message: 'Add Movie has success',
-        }
-
-        response.redirect('/addMovies')
-      })
-
-      conn.release()
-   })
-
-})
-
-router.get('/addGenre', function (request, response){
-  if (request.session.isAdmin){
-    
-    response.render("admin/addGenre", {title: "add movie genre", isAdmin: request.session.isAdmin})
-  }
-  else {
-    response.redirect('/login')
-  }
-})
-
-router.post('/addGenre', function (request, response){
-  const {genre} = request.body
-
-  const query = `INSERT INTO tb_type (name) VALUES ("${genre}")`
-  dbConnection.getConnection(function(err,conn){
-    if(err) throw err;
-      conn.query(query,function(err,result) {
-        if (err) throw err;
-
-        request.session.message = {
-          type: 'success',
-          message: 'Add genre has success',
-        }
-
-        response.redirect('/addGenre');
-      })
-
-      conn.release();
-   })
-
-})
-
-router.get('/detail/:id', function(request,response) {
-  const id = request.params.id;
-  const query = `SELECT * FROM tb_movie WHERE id = ${id}`;
-
-  dbConnection.getConnection(function (err,conn) {
+  dbConnection.getConnection(function (err, conn) {
     if (err) throw err;
     conn.query(query, function (err, results) {
       if (err) throw err;
@@ -201,30 +22,200 @@ router.get('/detail/:id', function(request,response) {
           name: result.name,
           duration: result.movie_hour,
           poster: pathFile + result.poster,
-          sinopsis: result.sinopsis
         });
       }
 
-      if (request.session.isAdmin){
-        response.render("admin/detail", {
-        title: "Movies detail", 
-        isAdmin: request.session.isAdmin,
-        movies
-      })
+      if (request.session.isAdmin) {
+        response.render("admin/dashboard", {
+          title: "dashboard",
+          isAdmin: request.session.isAdmin,
+          movies,
+        });
+      } else {
+        response.redirect("/login");
       }
-      else {
-        response.redirect('/login')
-      }
-
-      });
+      conn.release();
     });
-})
+  });
+});
 
-router.get('/edit/:id', function (request, response){
+router.get("/reports", function (request, response) {
+  const query = `SELECT t.id, t.ticket_number, t.date_show, p.sub_total, m.name, u.email
+  FROM tb_ticket t
+  INNER JOIN tb_movie m
+  ON t.movie_id = m.id
+  INNER JOIN tb_payment p
+  ON t.id = p.ticket_id
+  INNER JOIN tb_user u
+  ON t.user_id = u.user_id;`;
+
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, results) {
+      if (err) throw err;
+      const reports = [];
+
+      for (var result of results) {
+        reports.push({
+          id: result.id,
+          ticket: result.ticket_number,
+          name: result.name,
+          date: result.date_show,
+          email: result.email,
+          total: result.sub_total,
+        });
+      }
+      if (request.session.isAdmin) {
+        response.render("admin/reports", {
+          title: "dashboard",
+          isAdmin: request.session.isAdmin,
+          reports,
+        });
+      } else {
+        response.redirect("/login");
+      }
+      conn.release();
+    });
+  });
+});
+
+router.get("/addMovies", function (request, response) {
+  const query = `SELECT id, name FROM tb_type`;
+
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, results) {
+      if (err) throw err;
+      const type = [];
+
+      for (var result of results) {
+        type.push({
+          id: result.id,
+          name: result.name,
+        });
+      }
+
+      if (request.session.isAdmin) {
+        response.render("admin/addMovies", {
+          title: "add new movies",
+          isAdmin: request.session.isAdmin,
+          type,
+        });
+      } else {
+        response.redirect("/login");
+      }
+
+      conn.release();
+    });
+  });
+});
+
+router.post("/addMovies", uploadFile("image"), function (request, response) {
+  const { movie_title, duration, sinopsis, category } = request.body;
+  var image = "";
+
+  // handle file
+  if (request.file) {
+    image = request.file.filename;
+  }
+
+  if (movie_title == "" || image == "" || duration == "") {
+    request.session.message = {
+      type: "danger",
+      message: "Please insert all field!",
+    };
+
+    response.redirect("/addMovies");
+  }
+
+  const query = `INSERT INTO tb_movie (name, movie_hour, poster, sinopsis, type_id) VALUES ("${movie_title}", "${duration}", "${image}", "${sinopsis}", "${category}")`;
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, result) {
+      if (err) throw err;
+
+      request.session.message = {
+        type: "success",
+        message: "Add Movie has success",
+      };
+
+      response.redirect("/addMovies");
+    });
+
+    conn.release();
+  });
+});
+
+router.get("/addGenre", function (request, response) {
+  if (request.session.isAdmin) {
+    response.render("admin/addGenre", {
+      title: "add movie genre",
+      isAdmin: request.session.isAdmin,
+    });
+  } else {
+    response.redirect("/login");
+  }
+});
+
+router.post("/addGenre", function (request, response) {
+  const { genre } = request.body;
+
+  const query = `INSERT INTO tb_type (name) VALUES ("${genre}")`;
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, result) {
+      if (err) throw err;
+
+      request.session.message = {
+        type: "success",
+        message: "Add genre has success",
+      };
+
+      response.redirect("/addGenre");
+    });
+
+    conn.release();
+  });
+});
+
+router.get("/detail/:id", function (request, response) {
   const id = request.params.id;
   const query = `SELECT * FROM tb_movie WHERE id = ${id}`;
-  const movies = []
-  dbConnection.getConnection(function (err,conn) {
+
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, results) {
+      if (err) throw err;
+      const movies = [];
+
+      for (var result of results) {
+        movies.push({
+          id: result.id,
+          name: result.name,
+          duration: result.movie_hour,
+          poster: pathFile + result.poster,
+          sinopsis: result.sinopsis,
+        });
+      }
+
+      if (request.session.isAdmin) {
+        response.render("admin/detail", {
+          title: "Movies detail",
+          isAdmin: request.session.isAdmin,
+          movies,
+        });
+      } else {
+        response.redirect("/login");
+      }
+    });
+  });
+});
+
+router.get("/edit/:id", function (request, response) {
+  const id = request.params.id;
+  const query = `SELECT * FROM tb_movie WHERE id = ${id}`;
+  const movies = [];
+  dbConnection.getConnection(function (err, conn) {
     if (err) throw err;
     conn.query(query, function (err, results) {
       if (err) throw err;
@@ -234,68 +225,65 @@ router.get('/edit/:id', function (request, response){
           id: result.id,
           name: result.name,
           duration: result.movie_hour,
-          poster: pathFile + result.poster
-        })
+          poster: pathFile + result.poster,
+        });
       }
 
-      if (request.session.isAdmin){
+      if (request.session.isAdmin) {
         response.render("admin/edit", {
-        title: "Edit Movies", 
-        isAdmin: request.session.isAdmin,
-        movies
-      })
+          title: "Edit Movies",
+          isAdmin: request.session.isAdmin,
+          movies,
+        });
+      } else {
+        response.redirect("/login");
       }
-      else {
-        response.redirect('/login')
-      }
-
-      })
-    })
-})
-
-router.post('/edit', uploadFile('image'), function(request,response) {
-  const {movie_title, duration, oldPoster, id, sinopsis} = request.body
-
-  var image = oldPoster.replace(pathFile, '')
-  if (request.file) {
-    image = request.file.filename
-  }
-
-  const query = `UPDATE tb_movie SET name = "${movie_title}", movie_hour = "${duration}", poster = "${image}", sinopsis = "${sinopsis}" WHERE id = "${id}"`
-
-  dbConnection.getConnection(function(err,conn){
-    if(err) throw err
-      conn.query(query,function(err,results) {
-        if (err) throw err
-        console.log(query)
-        response.redirect('/admin')
-      })
-      conn.release()
-   })
-})
-
-router.get('/delete/:id', function (request, response){
-  const id = request.params.id
-
-  const query = `DELETE FROM tb_movie WHERE id = ${id}`
-
-  dbConnection.getConnection(function(err,conn){
-    if(err) throw err
-    conn.query(query,function(err,result) {
-      if (err) throw err
-
-      response.redirect('/admin')
-    })
-
-    conn.release()
-  })
-  
-})
-
-//  handle logout
-router.get('/logout', function(request,response) {
-  request.session.destroy();
-  response.redirect('/');
+    });
+  });
 });
 
-module.exports = router
+router.post("/edit", uploadFile("image"), function (request, response) {
+  const { movie_title, duration, oldPoster, id, sinopsis } = request.body;
+
+  var image = oldPoster.replace(pathFile, "");
+  if (request.file) {
+    image = request.file.filename;
+  }
+
+  const query = `UPDATE tb_movie SET name = "${movie_title}", movie_hour = "${duration}", poster = "${image}", sinopsis = "${sinopsis}" WHERE id = "${id}"`;
+
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, results) {
+      if (err) throw err;
+      console.log(query);
+      response.redirect("/admin");
+    });
+    conn.release();
+  });
+});
+
+router.get("/delete/:id", function (request, response) {
+  const id = request.params.id;
+
+  const query = `DELETE FROM tb_movie WHERE id = ${id}`;
+
+  dbConnection.getConnection(function (err, conn) {
+    if (err) throw err;
+    conn.query(query, function (err, result) {
+      if (err) throw err;
+
+      response.redirect("/admin");
+    });
+
+    conn.release();
+  });
+});
+
+//  handle logout
+router.get("/logout", function (request, response) {
+  request.session.destroy();
+  response.redirect("/");
+});
+
+module.exports = router;
